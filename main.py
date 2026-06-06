8551169b01c644696a3737e4fbd5b7fa1be371a5
devmanro <66820352+devmanro@users.noreply.github.com>
2026-06-06 23:31:02 +0100

    with drive update

--- main.py
+++ main.py
@@ -2,14 +2,30 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
 import joblib
+import os
+import requests
+import gdown
 
 from dictionary_rules import apply_dictionary
 
 app = FastAPI()
 
+MODEL_PATH = "marchandise_model.pkl"
+FILE_ID = "https://drive.google.com/file/d/1SU532bppGcmOCX_SJnsZgdKmx3jzuwCI/view?usp=drivesdk"
+
+def download_model():
+    if not os.path.exists(MODEL_PATH):
+        print("📥 Downloading model from Google Drive...")
+        url = f"https://drive.google.com/uc?id={FILE_ID}"
+        gdown.download(url, MODEL_PATH, quiet=False)
+        print("✅ Model downloaded!")
+
+download_model()
+
 print("Loading model...")
-model = joblib.load("marchandise_model.pkl")
+model = joblib.load(MODEL_PATH)
 print("✅ Model loaded")
+
 
 
 class Item(BaseModel):
